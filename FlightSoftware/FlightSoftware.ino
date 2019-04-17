df26f2cccbaf758a5f703eaeebaf4eb8b15ea69d
danderson11235 <30841272+danderson11235@users.noreply.github.com>
2019-04-17 17:11:26 -0400

    Update FlightSoftware.ino

--- FlightSoftware/FlightSoftware.ino
+++ FlightSoftware/FlightSoftware.ino
@@ -1,11 +1,15 @@
+/*
+ * Version 1.2
+ */
 #include <Adafruit_LSM9DS1.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <SD.h>
 #include <SPI.h>
 #include <Adafruit_MPL3115A2.h>
 #include <Adafruit_GPS.h>
 
-
+int xbeeBaud = 9600;
 #define mySerial Serial1
 Adafruit_GPS GPS(&mySerial);
 float telem_gpslat;
@@ -13,17 +17,27 @@ float telem_gpslon;
 float telem_gpssats;
 float telem_gpsalt;
 float telem_gpstime;
+
+File myFile;
+const int chipSelect = BUILTIN_SDCARD;
   
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
 Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
 
+String sensorData;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
-  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
-  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  Serial.begin(115200);
+  Serial2.begin(xbeeBaud);
+
+            //I dont know what these lines do. Something about calibration. either way
+            //never un commnt them.
+            //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+            //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+            //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+            
   if (! baro.begin()) Serial.print("Barometer Failed");
   if (!lsm.begin()) Serial.print("Accelerometer Failed");
   
@@ -32,14 +46,16 @@ void setup() {
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PGCMD_ANTENNA);
 
-  
+  if(!SD.begin(chipSelect))Serial.println("SD failed");
+  myFile = SD.open("data.txt", FILE_WRITE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(getSensorData());
+  sensorData = getSensorData();
+  Serial.println(sensorData);
+  Serial2.print(sensorData);
   delay(100);
-  
+  myFile.println(sensorData);
 }
 
 String getSensorData() {
@@ -76,4 +92,3 @@ String getSensorData() {
     " accX " + (String)accX + " magX " + (String)magX + " gyrX " + (String)gyrX;
   return answer;
 } 
-
